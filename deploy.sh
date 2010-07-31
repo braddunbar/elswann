2316511ab2c3cecdde0529452ca68493766826b1
braddunbar <dunbarb2@gmail.com>
2010-07-31 17:48:52 -0400

    rewrite mtimes.json before update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-tup upd && python2.5 /usr/local/google_appengine/appcfg.py --verbose update web
+#rm mtimes so it is rewritten before update (tup doesn't quite catch it)
+rm web/mtimes.json && tup upd && python2.5 /usr/local/google_appengine/appcfg.py --verbose update web
